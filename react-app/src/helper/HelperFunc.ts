674fafaa28caf6a885d74e3b7094815af7aaf375
Ryan <ryanchoooooo@gmail.com>
2024-06-24 13:07:39 +0800

    renamed folder to be standardized

--- react-app/src/helper/HelperFunc.ts
+++ react-app/src/helper/HelperFunc.ts
@@ -1,32 +1,32 @@
 import { ApiResponse, RetrieveRecords } from "../networkAPI/NetworkCommands";
 
 // Shuffles the word dump
-export function PrepareWordDump(wordDump: string[]): string[]{
-    for(let i = 0; i < wordDump.length; ++i){
-        wordDump[i] = wordDump[i].replace(/\s/g, '');
-    }
-    // Start from the end of the array
-    for (let i = wordDump.length - 1; i > 0; i--) {
-        // Generate a random index between 0 and i (inclusive)
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        
-        // Swap elements at randomIndex and i
-        [wordDump[i], wordDump[randomIndex]] = [wordDump[randomIndex], wordDump[i]];
-    }
+export function PrepareWordDump(wordDump: string[]): string[] {
+  for (let i = 0; i < wordDump.length; ++i) {
+    wordDump[i] = wordDump[i].replace(/\s/g, "");
+  }
+  // Start from the end of the array
+  for (let i = wordDump.length - 1; i > 0; i--) {
+    // Generate a random index between 0 and i (inclusive)
+    const randomIndex = Math.floor(Math.random() * (i + 1));
 
-    return wordDump;
+    // Swap elements at randomIndex and i
+    [wordDump[i], wordDump[randomIndex]] = [wordDump[randomIndex], wordDump[i]];
+  }
+
+  return wordDump;
 }
 
 // Retrieve top 10 WPM records
-export function RetrieveTop10(data:ApiResponse[]) : ApiResponse[]{
-    try {
-        const sortedData = data.sort((a,b) => b.wpm - a.wpm);
-        const top10Records = sortedData.slice(0,20);
+export function RetrieveTop10(data: ApiResponse[]): ApiResponse[] {
+  try {
+    const sortedData = data.sort((a, b) => b.wpm - a.wpm);
+    const top10Records = sortedData.slice(0, 20);
 
-        console.log("Records:", data);
-        return top10Records;
-    } catch (error) {
-        console.error("Error fetching records:", error);
-        return [];
-    }
+    console.log("Records:", data);
+    return top10Records;
+  } catch (error) {
+    console.error("Error fetching records:", error);
+    return [];
+  }
 }
--- react-app/src/views/Highscore.tsx
+++ react-app/src/views/Highscore.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { ApiPostRequest, ApiResponse, RetrieveRecords, SendRecord } from "../networkAPI/NetworkCommands";
+import {
+  ApiPostRequest,
+  ApiResponse,
+  RetrieveRecords,
+  SendRecord,
+} from "../networkAPI/NetworkCommands";
 
 import Table from "../components/Table";
 import Button from "../components/Button";
@@ -9,61 +14,74 @@ import { useLocation } from "react-router-dom";
 import { RetrieveTop10 } from "../helper/HelperFunc";
 
 function Highscore() {
-    const [records, setRecords] = useState<ApiResponse[]>([]);
-    const [savePromptVisibility, setSavePromptVisibility] = useState(false);
-    const [wpm, setWpm] = useState(0);
+  const [records, setRecords] = useState<ApiResponse[]>([]);
+  const [savePromptVisibility, setSavePromptVisibility] = useState(false);
+  const [wpm, setWpm] = useState(0);
 
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        console.log("Effect triggered. savePromptVisibility:", savePromptVisibility, "wpm:", wpm);
-        fetchRecords();
-        console.log(wpm);
-    }, [savePromptVisibility, wpm]);
-
-    useEffect(() => {
-        // null checks
-        if (location.state !== null) {
-            const { Wpm } = location.state as { Wpm: number };
-            setWpm(Wpm);
-        }
-    }, [])
-
-    const fetchRecords = async () => {
-        try {
-            const data = await RetrieveRecords();
-            setRecords(RetrieveTop10(data));
-            // setRecords(data);
-            console.log("Records:", data);
-        } catch (error) {
-            console.error("Error fetching records:", error);
-        }
-    };
-
-    const setSavePromptVisibilityTrue = () => {
-        setSavePromptVisibility(true);
-    };
-
-    const handleSetSavePromptVisibilityFalse = () => {
-        setSavePromptVisibility(false);
-    };
-
-    return (
-        <div className="highscoreContainer">
-            {wpm !== 0 ?
-                <div className="highscoreWpmDiv">
-                    <p>Current WPM: {wpm}</p>
-                    <Button buttonContext="Save" handleOnClick={setSavePromptVisibilityTrue} />
-                    {savePromptVisibility && (
-                        <SavePrompt wpm={wpm} setSavePromptVisibilityFalse={handleSetSavePromptVisibilityFalse}
-                        />
-                    )}
-                </div> : <div />}
-
-            <h1 className="highscoreHeader">Records Table</h1>
-            <Table records={records} cssProp="tableStyle"></Table>
-        </div>
+  useEffect(() => {
+    console.log(
+      "Effect triggered. savePromptVisibility:",
+      savePromptVisibility,
+      "wpm:",
+      wpm
     );
+    fetchRecords();
+    console.log(wpm);
+  }, [savePromptVisibility, wpm]);
+
+  useEffect(() => {
+    // null checks
+    if (location.state !== null) {
+      const { Wpm } = location.state as { Wpm: number };
+      setWpm(Wpm);
+    }
+  }, []);
+
+  const fetchRecords = async () => {
+    try {
+      const data = await RetrieveRecords();
+      setRecords(RetrieveTop10(data));
+      // setRecords(data);
+      console.log("Records:", data);
+    } catch (error) {
+      console.error("Error fetching records:", error);
+    }
+  };
+
+  const setSavePromptVisibilityTrue = () => {
+    setSavePromptVisibility(true);
+  };
+
+  const handleSetSavePromptVisibilityFalse = () => {
+    setSavePromptVisibility(false);
+  };
+
+  return (
+    <div className="highscoreContainer">
+      {wpm !== 0 ? (
+        <div className="highscoreWpmDiv">
+          <p>Current WPM: {wpm}</p>
+          <Button
+            buttonContext="Save"
+            handleOnClick={setSavePromptVisibilityTrue}
+          />
+          {savePromptVisibility && (
+            <SavePrompt
+              wpm={wpm}
+              setSavePromptVisibilityFalse={handleSetSavePromptVisibilityFalse}
+            />
+          )}
+        </div>
+      ) : (
+        <div />
+      )}
+
+      <h1 className="highscoreHeader">Records Table</h1>
+      <Table records={records} cssProp="tableStyle"></Table>
+    </div>
+  );
 }
 
 export default Highscore;
